86403c426ac0d5f75ac47a1a2f76eaa63a3aed0c
Your name <your.email@example.com>
2019-06-13 09:31:41 +0200

    Initial Commit

---
+++ LetsMoveSwift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LetsMoveSwift
+//
+//  Created by thierry hentic on 13/06/2019.
+//  Copyright © 2019 thierry hentic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
